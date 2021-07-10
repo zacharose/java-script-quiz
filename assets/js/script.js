cfbc609eb5c9fc0b1af6618e1edfb65cf5892cc0
Zach rose <zachrose@ymail.com>
2021-07-10 17:31:10 -0500

    questions array added

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// array to hold questions.
+var questions = [
+    { q:'', a:''},
+    { q:'', a:''},
+    { q:'', a:''},
+    { q:'', a:''},
+    { q:'', a:''},
+    { q:'', a:''},
+    { q:'', a:''},
+    { q:'', a:''},
+    { q:'', a:''},
+    { q:'', a:''},
+    
+];
+
+// start game at 0
+var score = 0;
+
+// loop over every question
+for (var i = 0; i < questions.length; i++) {
+    
+}
