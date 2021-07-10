48f49afff942c375ce10e3243a2a4fc138bda5b6
Zach rose <zachrose@ymail.com>
2021-07-10 17:55:44 -0500

    quiz start function complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,34 @@
-// array to hold questions.
-var questions = [
-    { q:'', a:''},
-    { q:'', a:''},
-    { q:'', a:''},
-    { q:'', a:''},
-    { q:'', a:''},
-    { q:'', a:''},
-    { q:'', a:''},
-    { q:'', a:''},
-    { q:'', a:''},
-    { q:'', a:''},
-    
-];
-
+var btnEl = document.querySelector(".btn")
 // start game at 0
 var score = 0;
-
+var startQuiz = function() {
+    // array to hold questions.
+var questions = [
+    { q:'1', a:'t'},
+    { q:'2', a:'t'},
+    { q:'3', a:'f'},
+    { q:'4', a:'f'},
+    { q:'5', a:'f'},
+    { q:'6', a:'t'},
+    { q:'7', a:'f'},
+    { q:'8', a:'t'},
+    { q:'9', a:'f'},
+    { q:'10', a:'f'},
+]
 // loop over every question
 for (var i = 0; i < questions.length; i++) {
-    
+    var answer = confirm(questions[i].q);
+
+     if (
+      (answer === true && questions[i.a === 't']) ||
+      (answer === false && questions[i.a === 'f'])
+     ) {
+     score++;
+     alert('Correct!');
+    } else {
+    alert('Wrong');
+     }
+    }
 }
+
+btnEl.addEventListener('click', startQuiz)
